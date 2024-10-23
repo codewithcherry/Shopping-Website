b00cf1381f6592600968c6160e13bb1e7ebc7471
Prashanth Naroju <prashanth.naro@gmail.com>
2024-10-23 14:23:22 +0530

    configure the corsoptions

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,17 @@
 const express = require('express');
+const cors=require('cors');
 const app = express();
 
+
+const corsOptions={
+  origin: ['http://localhost:3000', 'https://yourfrontenddomain.com'], // Replace with your allowed domains
+  methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific methods
+  credentials: true, // Allow cookies and credentials (like tokens)
+  allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept'], // Allow authorization and other headers
+}
+
+//cnfigure cors
+app.use(cors(corsOptions));
 // Define a simple route
 app.get('/', (req, res) => {
   res.send('Hello, World!');
