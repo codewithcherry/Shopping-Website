6f7976f82dc56a74db83547b7d120ed8d4ecf0dc
Prashanth Naroju <prashanth.naro@gmail.com>
2024-10-25 13:53:00 +0530

    change the products code based on frontend query pagination

--- Backend/controllers/shop.js
+++ Backend/controllers/shop.js
@@ -1,14 +1,28 @@
 const Product=require("../models/product");
 
 exports.getHome=(req,res,next)=>{
-    Product.find()
-    .then(products=>{
-        res.status(200).json({
-            success: true,
-            products: products
-          });
+    const page=req.query.page;
+    const limit=8;
+    Product.countDocuments().then(totaldoc=>{
+       const pagination={
+        total:totaldoc,
+        totalPages:Math.ceil(totaldoc/limit),
+        currentpage:page,
+        nextPage:page<(Math.ceil(totaldoc/limit))?true:false,
+        prevPage:page>1?true:false
+       }
+       return    Product.find()
+                        .skip((page-1)*limit)
+                        .limit(limit)
+                        .then(products=>{
+                            res.status(200).json({
+                                success: true,
+                                products: products,
+                                pagination:pagination
+                                });
+                        })
     })
     .catch(err=>{
-        console.log(error);
+        console.log(err);
     })
 }
