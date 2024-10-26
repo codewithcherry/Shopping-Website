5755d7287391b91ff5d4671e2fa8d38cd9311d66
Prashanth Naroju <prashanth.naro@gmail.com>
2024-10-26 14:11:16 +0530

    create orders and cart pages

---
+++ Frontend/src/pages/Cart.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const Cart = () => {
+  return (
+    <div>
+        <Navbar />
+        <h1>This is cart page</h1>
+    </div>
+  )
+}
+
+export default Cart
