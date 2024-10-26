12d2cf5f0f87491caf3478f37ef8ddd7b6ef7684
Prashanth Naroju <prashanth.naro@gmail.com>
2024-10-26 13:14:34 +0530

    set the jwt token and send response on successfull login

--- Backend/controllers/auth.js
+++ Backend/controllers/auth.js
@@ -1,5 +1,7 @@
 const User = require("../models/user");
 const bcrypt = require('bcrypt');
+const jwt_secret=process.env.JWT_SECRET;
+const jwt=require('jsonwebtoken')
 
 exports.registerAccount = async (req, res, next) => {
   const { email, password } = req.body;
@@ -46,7 +48,15 @@ exports.loginToAccount = async (req, res, next) => {
     }
     const isMatch = await bcrypt.compare(password, user.password); // Compare with the user's hashed password
     if (isMatch) {
-      return res.status(200).json({type:"success", message: "Login successful" });
+      // Generate a JWT with the user info
+    const token = jwt.sign(
+      { userId: user._id, useremail: user.useremail}, // Payload (user info)
+      jwt_secret, // Secret key
+      { expiresIn: '1h' } // Token expiry time
+    );
+
+    // Send the token to the front end
+      return res.status(200).json({type:"success", message: "Login successful",token:token });
     } else {
       return res.status(401).json({ type:"error",message: "Password does not match" });
     }
