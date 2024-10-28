d60affbec83fb4fc6be9eb3ae4c8b199ce8836d0
Prashanth Naroju <prashanth.naro@gmail.com>
2024-10-28 14:19:48 +0530

    create admin page

---
+++ Frontend/src/pages/Admin/Admin.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import AdminLogin from '../../components/Admin/AdminLogin'
+
+const Admin = () => {
+  return (
+    <div>
+      <AdminLogin />
+    </div>
+  )
+}
+
+export default Admin
