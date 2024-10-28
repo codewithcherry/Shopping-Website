a3844d6df3f1e4a8547bf1b01ff535d29d7577a8
Prashanth Naroju <prashanth.naro@gmail.com>
2024-10-28 21:48:24 +0530

    admin home dashboard get request middleware

--- Backend/routes/adminRoute.js
+++ Backend/routes/adminRoute.js
@@ -1,7 +1,9 @@
 const express=require("express");
 const adminController=require('../controllers/admin');
+const validateController=require("../controllers/validate");
 const router=express.Router();
 
 router.post("/login",adminController.adminLogin);
+router.get("/dashboard",validateController.adminAuthenticate,adminController.getDashboard);
 
 module.exports=router;
