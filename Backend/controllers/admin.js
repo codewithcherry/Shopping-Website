15df3ec338c089197df17864a217246f34c4301e
Prashanth Naroju <prashanth.naro@gmail.com>
2024-10-28 15:37:20 +0530

    code the post login middleware for admin

---
+++ Backend/controllers/admin.js
@@ -0,0 +1,32 @@
+const Admin = require("../models/admin");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const jwt_secret=process.env.JWT_SECRET
+
+exports.adminLogin = async (req, res, next) => {
+    const { username, password } = req.body;
+    // console.log(username, password);
+    try {
+        const admin = await Admin.findOne({ username: username });
+        if (!admin) {
+            return res.status(404).json({ type: "error", message: 'Admin does not exist' });
+        }
+        
+        const isMatch = await bcrypt.compare(password, admin.password);
+        
+        if (!isMatch) {
+            return res.status(400).json({type:"error",message:"Password does not match"});
+        }
+
+        // Authentication successful
+        const token = jwt.sign(
+            { user:admin}, // Payload (user info)
+            jwt_secret, // Secret key
+            { expiresIn: '1h' } )
+
+        return res.status(200).json({type:"success",message:"Login successful" ,token:token});
+
+    } catch (err) {
+        return res.status(500).json({type:"error",message:"Internal server error"});
+    }
+};
