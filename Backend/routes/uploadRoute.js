4a0729fd313532b7a4bb540aaf9c48f9365eb314
Prashanth Naroju <prashanth.naro@gmail.com>
2024-10-30 04:55:00 +0530

    make router for add product images

---
+++ Backend/routes/uploadRoute.js
@@ -0,0 +1,28 @@
+const express=require("express");
+const uploadController=require("../controllers/upload");
+const multer=require('multer')
+
+const router=express.Router()
+
+// Set up Multer with memory storage
+const storage = multer.memoryStorage();
+
+
+// Filter files based on MIME type
+const fileFilter = (req, file, cb) => {
+    // Allowed MIME types
+    const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/gif'];
+  
+    if (allowedMimeTypes.includes(file.mimetype)) {
+      // Accept the file if it's a valid type
+      cb(null, true);
+    } else {
+      // Reject the file if it's not a valid type
+      cb(new Error('Invalid file type. Only JPEG, PNG, and GIF are allowed.'), false);
+    }
+}
+    const upload = multer({ storage ,fileFilter});
+
+router.post("/add-product-image", upload.single('image'),uploadController.uploadProductImage);
+
+module.exports=router
