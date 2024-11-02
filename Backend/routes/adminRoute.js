afd6f602b9efb62538c12674f39298d8e7abf441
Prashanth Naroju <prashanth.naro@gmail.com>
2024-11-02 20:21:38 +0530

    add get admin products route

--- Backend/routes/adminRoute.js
+++ Backend/routes/adminRoute.js
@@ -5,6 +5,7 @@ const router=express.Router();
 
 router.post("/login",adminController.adminLogin);
 router.get("/dashboard",validateController.adminAuthenticate,adminController.getDashboard);
+router.get("/products",validateController.adminAuthenticate,adminController.getProducts);
 router.post("/add-product",validateController.adminAuthenticate,adminController.addProduct);
 
 module.exports=router;
