227c37895bc6a6f35f4c8028bcabeaa1d37899b9
Prashanth Naroju <prashanth.naro@gmail.com>
2024-11-02 20:22:18 +0530

    create and code getProducts middle for admin controller

--- Backend/controllers/admin.js
+++ Backend/controllers/admin.js
@@ -78,3 +78,30 @@ exports.getDashboard=(req,res,next)=>{
     const user=req.user;
     console.log("dashboard rendered");
 }
+
+exports.getProducts=(req,res,next)=>{
+    const page=req.query.page;
+    const limit=8;
+    Product.countDocuments().then(totaldoc=>{
+       const pagination={
+        total:totaldoc,
+        totalPages:Math.ceil(totaldoc/limit),
+        currentpage:page,
+        nextPage:page<(Math.ceil(totaldoc/limit))?true:false,
+        prevPage:page>1?true:false
+       }
+       return    Product.find()
+                        .skip((page-1)*limit)
+                        .limit(limit)
+                        .then(products=>{
+                            res.status(200).json({
+                                success: true,
+                                products: products,
+                                pagination:pagination
+                                });
+                        })
+    })
+    .catch(err=>{
+        res.status(500).json({type:"error",message:"Internal Server Error"})
+    })
+}
