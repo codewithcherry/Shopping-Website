5b9c671650a9eb1f7e0df429ba467eace1ac82e7
Prashanth Naroju <prashanth.naro@gmail.com>
2024-11-02 17:12:11 +0530

    create product details page and made api call to get the data from server

---
+++ Frontend/src/pages/ProductDetails.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import Navbar from '../components/navbar';
+import { useParams,useNavigate } from 'react-router-dom';
+import { useState,useEffect } from 'react';
+import ImageGallery from '../components/products/product/ImageGallery';
+import ProductSummary from '../components/products/product/ProductSummary';
+import axios from 'axios';
+
+const ProductDetails = () => {
+    const { productId } = useParams(); // Extract the ID from the URL
+    const [product, setProduct] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    const navigate =useNavigate()
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            try {
+                const response = await axios.get(`http://localhost:3000/products/product/${productId}`);
+                setProduct(response.data);
+            } catch (err) {
+                setError(err.message);
+                navigate("/products")
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        fetchProduct();
+    }, [productId]);
+    return (
+        <div className='bg-gray-100 min-h-screen flex flex-col'>
+            <Navbar />
+            {
+                loading?" ":<div className='flex-grow flex justify-center py-10'>
+                <div className='container mx-auto flex flex-col lg:flex-row justify-center gap-8'>       
+                        <ImageGallery images={product.images}/>              
+                        <ProductSummary product={product}/>          
+                </div>
+            </div>
+            }
+            
+        </div>
+    );
+};
+
+export default ProductDetails;
