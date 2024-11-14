f37c1178c37e937b0031f82cb8e7ea92d71fa9ca
Prashanth Naroju <prashanth.naro@gmail.com>
2024-11-14 15:20:38 +0530

    create route to post request for generate invoice

--- Backend/routes/orderRoute.js
+++ Backend/routes/orderRoute.js
@@ -5,6 +5,7 @@ const orderController=require('../controllers/order')
 const validateController=require('../controllers/validate')
 
 router.post("/place-order",validateController.authenticate,orderController.placeOrder);
-router.get("/get-user-orders",validateController.authenticate,orderController.getUserOrders)
+router.get("/get-user-orders",validateController.authenticate,orderController.getUserOrders);
+router.post('/generate-invoice',validateController.authenticate,orderController.generateInvoice);
 
 module.exports=router
