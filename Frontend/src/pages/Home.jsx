792515dff9e74eddc954ca941cc916ecd8cab8b8
Prashanth Naroju <prashanth.naro@gmail.com>
2024-11-26 16:19:59 +0530

    add all the home page components to home page

--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,10 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navigation/Navbar'
-import ImageCarousel from '../components/home/ImageCarousel'
+import RecentlyViewedProducts from '../components/products/product/RecentlyViewedProducts';
 import DealCarousel from '../components/home/DealCarousel'
 import Categories from '../components/home/Categories'
 import Footer from '../components/Footer/Footer'
 import FlashSale from '../components/products/product/FlashSale'
+import Promo1 from '../components/home/Promo1'
+import BestSellingProducts from '../components/products/product/BestSellingProducts'
+import ExploreProducts from '../components/home/ExploreProducts'
+import NewArrival from '../components/home/NewArrival'
+import Features from '../components/home/Features';
 
 
 const Home = () => {
@@ -14,6 +19,12 @@ const Home = () => {
       <DealCarousel />
       <Categories />
       <FlashSale />  
+      <Promo1 />
+      <BestSellingProducts />
+      <NewArrival />
+      <ExploreProducts />
+      <RecentlyViewedProducts />
+      <Features />
       <Footer /> 
     </div>
   )
