52c20919914976596fb56acaf5648a1086039618
Prashanth Naroju <prashanth.naro@gmail.com>
2024-12-02 21:15:24 +0530

    change the page to render the products based on search query from user

--- Frontend/src/pages/Products.jsx
+++ Frontend/src/pages/Products.jsx
@@ -4,6 +4,7 @@ import { useLocation } from "react-router-dom";
 import { useState, useEffect } from "react";
 import ProductCard from "../components/products/ProductCard";
 import axios from "axios";
+import Breadcrumbs from '../components/Navigation/BreadCrumbs';
 import ProductPagination from "../components/pagination/ProductPagination";
 import Footer from "../components/Footer/Footer";
 import Loading from "../components/Alert/Loading";
@@ -14,8 +15,15 @@ const Products = () => {
   const [pagination, setPagination] = useState({});
   const [page, setPage] = useState(1);
 
+
   const useQuery = () => new URLSearchParams(useLocation().search);
   const query = useQuery().get("query");
+
+  const breadcrumbs = [
+    { label: 'Home', link: '/' },
+    
+    { label: "products", link: `/products?query=${query}` },
+   ]
 
   const handlePagination = (page) => {
     console.log("page updated");
@@ -47,17 +55,20 @@ const Products = () => {
       <Navbar />
       <div>
         <div>
-          <h1 className="text-3xl font-semibold text-center text-indigo-700 m-4 ">
-            Products
-          </h1>
+        <div className='flex gap-8 items-center mt-6 mx-6'>
+                <Breadcrumbs breadcrumbs={breadcrumbs}/>
+                <p className="text-sm">Search results for <span className="text-blue-600 font-medium">{`${query}`}</span></p>
+        </div>
         </div>
         {loading ? (
           <Loading />
         ) : (
           <div className="flex justify-center w-full">
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:w-[80%] lg:grid-cols-4 gap-8 p-4">
+            <div className={`${products.length == 0?"flex":"grid"} grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:w-[80%] lg:grid-cols-4 gap-8 p-4`}>
               {products.length == 0 ? (
-                <h1>no products</h1>
+                <div className="mx-auto">
+                  <h1 className="text-lg font-medium text-gray-600 ">No products to display</h1>
+                </div>
               ) : (
                 products.map((product) => (
                   <ProductCard key={product._id} product={product} />
