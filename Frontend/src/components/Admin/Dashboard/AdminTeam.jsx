92921b6ab01448a4aa2c56b29cfcd5e416d34082
Prashanth Naroju <prashanth.naro@gmail.com>
2024-12-10 22:14:39 +0530

    code the add new teammate functionality for admin team page

--- Frontend/src/components/Admin/Dashboard/AdminTeam.jsx
+++ Frontend/src/components/Admin/Dashboard/AdminTeam.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
+import AddTeamForm from '../Dashboard/components/Team/AddTeamForm';
+import { PlusIcon } from '@heroicons/react/24/solid';
+import Alert from '../../Alert/Alert'
 
 const AdminTeam = () => {
-  return (
-    <div>
-      Admin Team page
-    </div>
-  )
-}
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [alert,setAlert]=useState();
 
-export default AdminTeam
+  const toggleModal = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
+  return (
+    <div className="p-6">
+     <div className='w-full z-20'> {alert && <Alert type={alert.type} message={alert.message} onClose={()=>setAlert(null)} />}</div>
+      <div className="flex justify-end">
+        <button
+          onClick={toggleModal}
+          className="flex items-center gap-1 bg-indigo-500 text-white px-4 py-2 rounded-lg shadow-md hover:bg-indigo-600"
+        >
+          <PlusIcon className='w-5 h-5 text-white'/>
+          Add Teammate
+        </button>
+      </div>
+      {isModalOpen && <AddTeamForm closeModal={toggleModal} setAlert={setAlert} />}
+    </div>
+  );
+};
+
+export default AdminTeam;
