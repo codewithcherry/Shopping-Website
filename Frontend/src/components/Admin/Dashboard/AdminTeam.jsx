0ea6aec0c07bf7a9d76c525e13dce006d6446aa0
Prashanth Naroju <prashanth.naro@gmail.com>
2024-12-11 16:56:25 +0530

    dashboard team page create fetchteamates() function with get request and add teamMembercard to map all admins

--- Frontend/src/components/Admin/Dashboard/AdminTeam.jsx
+++ Frontend/src/components/Admin/Dashboard/AdminTeam.jsx
@@ -1,18 +1,49 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AddTeamForm from '../Dashboard/components/Team/AddTeamForm';
-import { PlusIcon } from '@heroicons/react/24/solid';
+import { PlusIcon,UserIcon } from '@heroicons/react/24/solid';
 import Alert from '../../Alert/Alert'
+import TeamMemberCard from './components/Team/TeamMemberCard';
+import axios from 'axios';
+import Loading from '../../Alert/Loading';
 
 const AdminTeam = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [alert,setAlert]=useState();
+  const [teamMembers,setTeamMembers]=useState([]);
+  const [loading,setLoading]=useState(false);
+
+  const adminToken=localStorage.getItem('adminToken')
+
+  const fetchTeamMembers=async () => {
+    try {
+      setLoading(true)
+      const response=await axios.get('http://localhost:3000/admin/get-team-members',
+        {
+          headers:{
+            Authorization:`Bearer ${adminToken}`
+          }
+        }
+      )
+      setTeamMembers(response.data)
+      console.log(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+    finally{
+      setLoading(false)
+    }
+  }
+
+  useEffect(()=>{
+    fetchTeamMembers()
+  },[])
 
   const toggleModal = () => {
     setIsModalOpen(!isModalOpen);
   };
 
   return (
-    <div className="p-6">
+    <div className="p-6 w-full bg-gray-100">
      <div className='w-full z-20'> {alert && <Alert type={alert.type} message={alert.message} onClose={()=>setAlert(null)} />}</div>
       <div className="flex justify-end">
         <button
@@ -24,6 +55,19 @@ const AdminTeam = () => {
         </button>
       </div>
       {isModalOpen && <AddTeamForm closeModal={toggleModal} setAlert={setAlert} />}
+      <div className='text-xl text-gray-700 font-medium mx-8 flex items-center gap-1 '><UserIcon className='w-5 h-5 text-gray-700'/> Team </div>
+      {loading?<Loading />:
+      <div className='w-full mx-auto p-6'>
+      {teamMembers.length==0?<div>
+       <p>No Team mates added</p>
+      </div>:
+      <div className=' grid grid-cols-4 gap-4'>
+       {teamMembers.map((member,index)=>{
+          return <TeamMemberCard key={index} image={member.imageUrl} firstName={member.firstname} role={member.role} lastName={member.lastname} email={member.email} phone={member.phone} socialLinks={{linkedin:member.linkedin,email:member.email,twitter:member.twitter,github:member.github}}/>
+       })}
+      </div>
+      }
+     </div>}
     </div>
   );
 };
