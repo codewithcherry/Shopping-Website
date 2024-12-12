10160dcc33d8c32b198ec9a43dd5ff55770bfa21
Prashanth Naroju <prashanth.naro@gmail.com>
2024-12-12 17:00:05 +0530

    create a route to admin to view product details

--- Backend/routes/adminRoute.js
+++ Backend/routes/adminRoute.js
@@ -1,11 +1,13 @@
 const express=require("express");
 const adminController=require('../controllers/admin');
+const shopController=require('../controllers/shop')
 const validateController=require("../controllers/validate");
 const router=express.Router();
 
 router.post("/login",adminController.adminLogin);
 router.get("/dashboard",validateController.adminAuthenticate,adminController.getDashboard);
 router.get("/products",validateController.adminAuthenticate,adminController.getProducts);
+router.get('/get-product/:productId',validateController.adminAuthenticate,shopController.getProductDetails);
 router.post("/add-product",validateController.adminAuthenticate,adminController.addProduct);
 router.get('/order-list',validateController.adminAuthenticate,adminController.getOrderlist);
 router.post('/add-team',validateController.adminAuthenticate,adminController.createTeam);
