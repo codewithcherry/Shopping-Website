77d54df35e6e700e9b305878deb0535f58cc407f
Prashanth Naroju <prashanth.naro@gmail.com>
2024-12-23 17:41:38 +0530

    add the header and chart component to home page

--- Frontend/src/components/Admin/Dashboard/AdminHome.jsx
+++ Frontend/src/components/Admin/Dashboard/AdminHome.jsx
@@ -3,50 +3,52 @@ import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Alert from '../../Alert/Alert';
 import axios from 'axios';
+import DashboardHomeHeader from './components/home/DashboardHomeHeader';
+import Loading from '../../Alert/Loading';
+import SalesChart from './components/home/SalesChart';
 
 const AdminHome = () => {
   
-  const navigate=useNavigate()
-
+    const navigate=useNavigate()
+    const [headerData,setHeaderData]=useState();
     const [alert,setAlert]= useState(false);
+    const token = localStorage.getItem('adminToken');
+
+    const fetchDashboardHeader=async()=>{
+     
+        try {
+          const config={headers: { "Authorization": `Bearer ${token}` }}
+          const response = await axios.get("http://localhost:3000/admin/dashboard", config);
+          console.log(response.data)
+          setHeaderData(response.data.data)
+        } catch (error) {
+          // Handle invalid token or server error
+          console.error("Token validation failed:", error);
+          setAlert({type:"error",message:"Internal Server error try again later"});
+        }
+      }
 
     useEffect(() => {
       console.log("dashboard home rendered")
-        const validateToken = async () => {
-          const token = localStorage.getItem('adminToken');
-          if (token) {
-            try {
-              const config={headers: { "Authorization": `Bearer ${token}` }}
-              const response = await axios.get("http://localhost:3000/admin/dashboard", config);
-              console.log(response.data)
-               // Assuming you have a state to track login
-            } catch (error) {
-              // Handle invalid token or server error
-              console.error("Token validation failed:", error);
-              setAlert({type:"error",message:"Internal Server error try again later"});
-            }
-          } else {           
-            navigate("/admin",{ 
-              state: { 
-                type:"info",message:"login to your acoount/session experied" 
-              } 
-          })
-            // No token found
-          }
-        };
-      
-        validateToken();
-        
+      fetchDashboardHeader();      
       }, []);
   return (
-    <>
-    <div className=' absolute top-0 right-0 w-full '>
+    <div className='bg-gray-100 w-full'>
     {alert&& <Alert type={alert.type } message={alert.message} onClose={()=>setAlert(null)}/>}
+   <div>
+    <h1 className='text-4xl font-semibold text-gray-700 mt-6 mx-10 p-2'>
+      Dashboard   
+    </h1>
+    <div className='mx-10'>
+       {
+        headerData?<DashboardHomeHeader headerData={headerData}/>:<Loading />
+       }
     </div>
-    <div className='bg-gray-100 w-full '>
-       this is home page of dashboard
+    <div className='mt-10 mx-10 p-2'>
+      <SalesChart setAlert={setAlert}/>
     </div>
-    </>
+   </div>
+    </div>
     
   )
 }
